aae7fcc310781ea900f30273bafc3b97184c727b
Mona Lisa <130613163+rudrijoshi@users.noreply.github.com>
2023-06-14 10:27:47 -0400

    added text to index.js

--- models/index.js
+++ models/index.js
@@ -11,22 +11,22 @@ newtweet.belongsTo(User, {
     foreignKey: 'user_id'
 });
 
-module.exports = { User, Blog };
+newtweet.hasMany(Comment, {
+    foreignKey: 'newtweet_id',
+    onDelete: 'CASCADE'
+});
 
-// Blog.hasMany(Comment, {
-//     foreignKey: 'blog_id',
-//     onDelete: 'CASCADE'
-// });
+Comment.belongsTo(newtweet, {
+    foreignKey: 'newtweet_id'
+});
 
-// Comment.belongsTo(Blog, {
-//     foreignKey: 'blog_id'
-// });
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
 
-// User.hasMany(Comment, {
-//     foreignKey: 'user_id',
-//     onDelete: 'CASCADE'
-// });
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
-// Comment.belongsTo(User, {
-//     foreignKey: 'user_id'
-// });
+module.exports = { User, newtweet, Comment };
