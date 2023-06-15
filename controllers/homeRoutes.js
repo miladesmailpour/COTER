8c73296b30e21d75e7c390a7bce65321d0128ed9
miladesmailpour <milad.esmailpour@gmail.com>
2023-06-15 13:30:55 -0400

    adding the homepage View

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,69 +1,69 @@
-const router = require('express').Router();
-const { Comment, User, Tweet } = require('../models');
+const router = require("express").Router();
+const { Comment, User, Tweet } = require("../models");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const tweetData = await Tweet.findAll({
       include: [
         {
           model: User,
-          attributes: ['username'],
+          attributes: ["username"],
         },
       ],
     });
     const tweets = tweetData.map((tweet) => tweet.get({ plain: true }));
 
-    res.render('homepage', {
+    res.render("homepage", {
       tweets,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/tweet/:id', async (req, res) => {
+router.get("/tweet/:id", async (req, res) => {
   try {
     const tweetData = await Tweet.findByPk(req.params.id, {
       include: [
         {
           model: User,
-          attributes: ['username'],
+          attributes: ["username"],
         },
       ],
     });
     const tweet = tweetData.get({ plain: true });
-    res.render('tweet', {
+    res.render("tweet", {
       ...tweet,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/profile', async (req, res) => {
+router.get("/profile", async (req, res) => {
   try {
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
+      attributes: { exclude: ["password"] },
       include: [{ model: Tweet }],
     });
     const user = userData.get({ plain: true });
-    res.render('profile', {
+    res.render("profile", {
       ...user,
-      logged_in: true
+      logged_in: true,
     });
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect("/profile");
     return;
   }
-  res.render('login');
+  res.render("login");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const routes = require("./controllers");
 const exphbs = require("express-handlebars");
 const sequelize = require("./config/connection");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
-const hbs = exphbs.create({  });
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 const app = express();
 const PORT = process.env.PORT || 3001;
 
