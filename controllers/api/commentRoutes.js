fae4c209bbf79f9d1f290e4520c5873249fbc5db
Andrew <andy.p.123@hotmail.com>
2023-06-17 15:08:29 -0400

    comments

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -12,20 +12,34 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.get('/:id', async (req, res) => {
-    try {
-        const commentData = await Comment.findAll({
-            where: {
-                id: req.params.id
-            }
-        });
-        res.json(commentData);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-    }
-});
+// router.get('/:id', async (req, res) => {
+//     try {
+//         const commentData = await Comment.findAll({
+//             where: {
+//                 id: req.params.id
+//             }
+//         });
+//         res.json(commentData);
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).json(err);
+//     }
+// });
 
+router.get('/tweet/:id', async (req, res) => {
+    try {
+      const commentsData = await Comment.findAll({
+        where: {
+          tweet_id: req.params.id,
+        },
+      });
+      const comments = commentsData.map((comment) => comment.get({ plain: true }));
+      res.status(200).json(comments);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+  
 router.post('/', withAuth, async (req, res) => {
     try {
         if (req.session) {
---
+++ public/js/comment-dropdown.js
@@ -0,0 +1,31 @@
+document.querySelectorAll('.comment-dropdown-btn').forEach((button) => {
+    button.addEventListener('click', async (event) => {
+      const tweetId = event.target.closest('#cote').dataset.id;
+      const dropdownMenu = event.target.nextElementSibling;
+  
+      if (dropdownMenu.classList.contains('show')) {
+        dropdownMenu.classList.remove('show');
+      } else {
+        // Fetch comments for the tweetId
+        const response = await fetch('/api/comments/1');
+        if (response.ok) {
+          const comments = await response.json();
+          renderComments(comments, dropdownMenu);
+          dropdownMenu.classList.add('show');
+        } else {
+          alert('Failed to fetch comments');
+        }
+      }
+    });
+  });
+  
+  function renderComments(comments, dropdownMenu) {
+    dropdownMenu.innerHTML = '';
+    comments.forEach((comment) => {
+      const commentElement = document.createElement('div');
+      commentElement.classList.add('dropdown-item');
+      commentElement.textContent = comment.comment_text;
+      dropdownMenu.appendChild(commentElement);
+    });
+  }
+  
